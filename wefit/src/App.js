a88d081d45ce9117ab241b10c998c0af49ecdd4d
Orli Riss <orli09@hotmail.com>
2022-05-18 14:39:54 +0300

    new start

--- wefit/src/App.js
+++ wefit/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from '../src/pages/Home'
 import Questionary from './pages/Questionary';
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "../src/pages/Home.jsx";
 import FormPage from "./pages/FormPage.jsx";
 import "../src/components/Form.css";
 
