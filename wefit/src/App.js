628b7057deca1a84336455d77e9ef1d7024beb8c
Orli Riss <orli09@hotmail.com>
2022-05-19 11:53:26 +0300

    new

--- wefit/src/App.js
+++ wefit/src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import Home from "../src/pages/Home";
 import { Route, Routes } from "react-router-dom";
 import FormPage from "./pages/FormPage.jsx";
+import Dashboard from "./pages/Dashboard";
+import CardBoard from "./pages/SwipePage";
+import Wrapper from "./pages/SwipePage";
+
 
 function App() {
   return (
@@ -9,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<FormPage />} />
+        
+
       </Routes>
     </div>
   );
---
+++ wefit/src/components/Card.jsx
@@ -0,0 +1,37 @@
+// import Card from "../pages/Dashboard";
+
+// const CardItem = () => {
+//     const cards = [
+//         {
+//         image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+//         color: '#55ccff'
+//         },
+//         {
+//         image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+//         color: '#e8e8e8'
+//         },
+//         {
+//         image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+//         color: '#0a043c'
+//         },
+//         {
+//         image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+//         color: 'black'
+//         }
+//     ];
+    
+//     return (
+//         <div className='App'>
+        
+//         {/* Traversing through cards arrray using map function
+//         and populating card with different image and color */}
+            
+//         {cards.map((card) => (
+//             <Card image={card.image} color={card.color} />
+//         ))}
+//         </div>
+//     );
+//     };
+
+//     export default CardItem;
+    
